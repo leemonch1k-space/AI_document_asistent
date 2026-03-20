6d8e4c410e99f9df959b87af1c3041b878c37c93
Oleh Skibinskyi <oleg.sk2002@gmail.com>
2026-03-20 18:50:20 +0200

    feature: add security && token services

---
+++ src/services/__init__.py
@@ -0,0 +1,8 @@
+from src.services.token import remove_expired_tokens
+from src.services.document import prepare_document
+
+
+__all__ = [
+    "remove_expired_tokens",
+    "prepare_document",
+]
---
+++ src/tasks/token_tasks.py
@@ -0,0 +1,21 @@
+import asyncio
+from src.config.celery_app import celery_instance
+from src.database.celery_session import task_db_session
+from src.database.models import RefreshTokenModel
+from src.services import remove_expired_tokens
+
+# Celery task for tokens
+
+
+async def _run_cleanup():
+    """Support method for celery task."""
+    async with task_db_session() as db:
+        await remove_expired_tokens(token_type=RefreshTokenModel, db=db)
+
+
+@celery_instance.task(name="remove_expired_refresh_tokens_task")
+def remove_expired_refresh_tokens_task() -> None:
+    """
+    Remove expired refresh tokens.
+    """
+    asyncio.run(_run_cleanup())
